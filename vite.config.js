a34eb762260637dcfe1603811e4a6fb0d45932d0
ElhamNakhkoob <elham_mn14@yahoo.com>
2025-05-23 15:25:29 +0200

    add new changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Elham.Nakhkoob-Portfolio/", // 👈 Add this line
   plugins: [react(), tailwindcss()],
 });
